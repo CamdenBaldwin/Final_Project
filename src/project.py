4eda1715b10182f387e5554111087b317e9248c3
Camden Baldwin <cjbaldwintx@gmail.com>
2025-04-16 21:21:32 -0500

    Made the walking animation clearer, Made it so when the player turns they keep facing the direction they stopped in, Added the houses, and changed the paths/grass to dither away

--- src/project.py
+++ src/project.py
@@ -2,57 +2,118 @@ import random
 import pygame
 import pygame.freetype
 
+class Houseroof():
+    def __init__(self, roof, x, y):
+        self.roof = roof
+        self.width = self.roof.get_width()
+        self.height = self.roof.get_height()
+        self.roof = pygame.transform.scale(self.roof, (self.width/2.5, self.height/2.5))
+        self.x = x
+        self.y = y
+    def update(self, direction):
+        if direction == "w":
+            self.y += 16
+        elif direction == "a":
+            self.x += 16
+        elif direction == "s":
+            self.y -= 16
+        elif direction == "d":
+            self.x -= 16
+        else:
+            pass
+
+    def draw(self, surface):
+        surface.blit(self.roof, (self.x, self.y))
+
+class Housebase():
+    def __init__(self, base, x, y):
+        self.base = base
+        self.width = self.base.get_width()
+        self.height = self.base.get_height()
+        self.base = pygame.transform.scale(self.base, (self.width/2.5, self.height/2.5))
+        self.x = x
+        self.y = y
+    
+    def update(self, direction):
+        if direction == "w":
+            self.y += 16
+        elif direction == "a":
+            self.x += 16
+        elif direction == "s":
+            self.y -= 16
+        elif direction == "d":
+            self.x -= 16
+        else:
+            pass
+    def draw(self, surface):
+        surface.blit(self.base, (self.x, self.y))
+
 class Paths():
     def __init__(self):
         self.background = pygame.image.load('images\\village\\pathsgrass.png')
-        width = self.background.get_width()
-        height = self.background.get_height()
-        self.background = pygame.transform.scale(self.background, (width/2.5, height/2.5))
-        self.x = -200
+        self.width = self.background.get_width()
+        self.height = self.background.get_height()
+        self.background = pygame.transform.scale(self.background, (self.width/2.5, self.height/2.5))
+        self.x = -285
         self.y = -1950
 
     def update(self, direction):
         if direction == "w":
-            self.y += 20
+            self.y += 16
         elif direction == "a":
-            self.x += 20
+            self.x += 16
         elif direction == "s":
-            self.y -= 20
+            self.y -= 16
         elif direction == "d":
-            self.x -= 20
+            self.x -= 16
         else:
             pass
+
     def draw(self, surface):
         surface.blit(self.background, (self.x, self.y))
 
 class Player():
 
     def __init__(self):
-        self.sprite = pygame.image.load('images\player\gloop.png')
+        self.sprite = pygame.image.load('images\\player\\gloopss.png')
     
-    def update(self, direction, walking):
+    def update(self, direction, lastkey, walking):
         if direction == "w":
-            if walking >= 13:
-                self.sprite = pygame.image.load('images\player\\back1.png')
+            if walking >= 7 and walking <=12:
+                self.sprite = pygame.image.load('images\\player\\back1.png')
+            elif walking >= 19:
+                self.sprite = pygame.image.load('images\\player\\back2.png')
             else:
-                self.sprite = pygame.image.load('images\player\\back2.png')
+                self.sprite = pygame.image.load('images\\player\\gloopws.png')
         elif direction == "a":
-            if walking >= 13:
-                self.sprite = pygame.image.load('images\player\left1.png')
+            if walking >= 7 and walking <=12:
+                self.sprite = pygame.image.load('images\\player\\left1.png')
+            elif walking >= 19:
+                self.sprite = pygame.image.load('images\\player\\left2.png')
             else:
-                self.sprite = pygame.image.load('images\player\left2.png')
+                self.sprite = pygame.image.load('images\\player\\gloopas.png')
         elif direction == "s":
-            if walking >= 13:
-                self.sprite = pygame.image.load('images\player\\walk1.png')
+            if walking >= 7 and walking <=12:
+                self.sprite = pygame.image.load('images\\player\\walk1.png')
+            elif walking >= 19:
+                self.sprite = pygame.image.load('images\\player\\walk2.png')
             else:
-                self.sprite = pygame.image.load('images\player\\walk2.png')
+                self.sprite = pygame.image.load('images\\player\\gloopss.png')
         elif direction == "d":
-            if walking >= 13:
-                self.sprite = pygame.image.load('images\player\\right1.png')
+            if walking >= 7 and walking <=12:
+                self.sprite = pygame.image.load('images\\player\\right1.png')
+            elif walking >= 19:
+                self.sprite = pygame.image.load('images\\player\\right2.png')
             else:
-                self.sprite = pygame.image.load('images\player\\right2.png')
+                self.sprite = pygame.image.load('images\\player\\gloopds.png')
+        elif lastkey == "w":
+            self.sprite = pygame.image.load('images\\player\\gloopws.png')
+        elif lastkey == "a":
+            self.sprite = pygame.image.load('images\\player\\gloopas.png')
+        elif lastkey == "d":
+            self.sprite = pygame.image.load('images\\player\\gloopds.png')
         else:
-            self.sprite = pygame.image.load('images\player\gloop.png')
+            self.sprite = pygame.image.load('images\\player\\gloopss.png')
         width = self.sprite.get_width()
         height = self.sprite.get_height()
         self.sprite = pygame.transform.scale(self.sprite, (width/2.2, height/2.2))
@@ -71,6 +132,14 @@ def main():
     player = Player()
     background = Paths()
     keydown = ""
+    lastkey = ""
+    house1base = Housebase(pygame.image.load('images\\village\\house1door.png'), 107, -158)
+    house2base = Housebase(pygame.image.load('images\\village\\house2door.png'), 1307, -158)
+    house3base = Housebase(pygame.image.load('images\\village\\house1door.png'), 2155, -150)
+    house1roof = Houseroof(pygame.image.load('images\\village\\house1roof.png'), 83, -366)
+    house2roof = Houseroof(pygame.image.load('images\\village\\house2roof.png'), 1267, -366)
+    house3roof = Houseroof(pygame.image.load('images\\village\\house1roof.png'), 2131, -358)
+    black = (0,0,0)
     walking = 0
     start_music()
     running = True
@@ -90,22 +159,39 @@ def main():
         keydown=""
         if pressed[pygame.K_w]:
             keydown = "w"
+            lastkey = "w"
         elif pressed[pygame.K_a]:
             keydown = "a"
+            lastkey = "a"
         elif pressed[pygame.K_s]:
             keydown = "s"
+            lastkey = "s"
         elif pressed[pygame.K_d]:
             keydown = "d"
+            lastkey = "d"
         # Game Logic
         walking += 1
         if walking >= 25:
             walking = 0
         displayInfo = pygame.display.Info()
         background.update(keydown)
-        player.update(keydown, walking)
+        house1base.update(keydown)
+        house2base.update(keydown)
+        house3base.update(keydown)
+        player.update(keydown, lastkey, walking)
+        house1roof.update(keydown)
+        house2roof.update(keydown)
+        house3roof.update(keydown)
         # Render and Display
+        screen.fill(black)
         background.draw(screen)
+        house1base.draw(screen)
+        house2base.draw(screen)
+        house3base.draw(screen)
         player.draw(screen, displayInfo)
+        house1roof.draw(screen)
+        house2roof.draw(screen)
+        house3roof.draw(screen)
         pygame.display.flip()
         dt = clock.tick(24)
     pygame.mixer.music.unload()
